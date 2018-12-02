67075907096b67d88667a8d528d0851215558bdd
Pierre Le Count <pmlc@iinet.net.au>
2018-12-02 05:03:18 +0000

    Main oage liks to Homepage and Documents working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { DocumentsComponent } from './documents/documents.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'documents', component: DocumentsComponent },
+  { path: 'homepage', component: HomepageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
