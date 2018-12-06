6ecb4f823bec82244ca322ec52a7efc985d89997
Pierre Le Count <pmlc@iinet.net.au>
2018-12-06 02:23:13 +0000

    Interim

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 
+/*import { environment } from '../../environments/environment';
+*/
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
